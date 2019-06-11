4107cd39d54a8c246fe6374d1c4db362d535a330
creperkiler2101 <50651572+creperkiler2101@users.noreply.github.com>
2019-06-11 09:33:17 +0300

    Add files via upload

---
+++ Project/EmailValidator.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Project
+{
+    public class EmailValidator
+    {
+        public static bool IsValidEmail(string email)
+        {
+            if (string.IsNullOrWhiteSpace(email)) return false;
+
+            string[] emailSplit = email.Split(new char[] { '@' }, StringSplitOptions.RemoveEmptyEntries);
+            if (emailSplit.Length != 2) return false;
+
+            string[] emailPart2Split = emailSplit[1].Split(new char[] { '.' }, StringSplitOptions.RemoveEmptyEntries);
+            if (emailPart2Split.Length != 2) return false;
+
+            return true;
+        }
+    }
+}
---
+++ Project/StringConverter.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+
+namespace Project
+{
+    public static class StringConverter
+    {
+        public static string GetBase64String(string original)
+        { return Convert.ToBase64String(Encoding.GetEncoding(1252).GetBytes(original)); }
+
+        public static string GetString(string base64String)
+        { return Encoding.GetEncoding(1252).GetString(Convert.FromBase64String(base64String)); }
+
+
+        private static char[] Symbols = new char[]
+        {
+            'a', 'A', 'b', 'B', 'c', 'C', 'd', 'D', 'e', 'E', 'f', 'F',
+            'g', 'G', 'h', 'H', 'i', 'I', 'j', 'J', 'k', 'K', 'l', 'L',
+            'm', 'M', 'n', 'N', 'o', 'O', 'p', 'P', 'q', 'Q', 'r', 'R',
+            's', 'S', 't', 'T', 'u', 'U', 'v', 'V', 'w', 'W', 'x', 'X',
+            'y', 'Y', 'z', 'Z', '0', '1', '2', '3', '4', '5', '6', '7',
+            '8', '9'
+        };
+
+        public static string GenerateRandomString()
+        {
+            string result = "";
+
+            Random random = new Random();
+            for (int i = 0; i < 32; i++)
+            {
+                int value_ = random.Next(0, Symbols.Length - 1);
+                result += Symbols[value_];
+            }
+
+            return result;
+        }
+    }
+}
